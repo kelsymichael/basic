b2d011b506f25ee46054f88685c85c37a346b35a
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-02-28 11:18:25 -0700

    Add gulp default exports

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
 const gulp = require('gulp');
 
+// second is where folder is located
+// each filename = a named task
+// as if we used gulp.task etc.
+require('gulp-load-tasks')('gulp_tasks');
+
 gulp.task('default', () => console.log('gulp is running!'));
