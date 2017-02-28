14ba387801aff89eb95c1acf99214458587fe969
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-02-28 11:31:11 -0700

    Configure jshint and test

---
+++ gulp_tasks/jshint.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+//module that exports a function
+// pipe is a node method, not gulp
+// start path with ! to exclude
+module.exports = () => gulp.src(['./src/**/*.js', '!./src/vendor/**/*.js'])
+                            .pipe(jshint('./config/.jshintrc'))
+                            .pipe(jshint.reporter(require('jshint-stylish')));
