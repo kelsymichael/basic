7a9da6f771d90e8ebe261c4c408dc8142e6c0aa7
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-02-28 11:41:56 -0700

    Add jscs config

---
+++ gulp_tasks/jscs.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp'),
+      jscs = require('gulp-jscs'),
+  reporter = require('gulp-jscs-stylish');
+
+module.exports = () => gulp.src('./src/**/*.js')
+                           .pipe(jscs({configPath: './config/.jscsrc', fix: true}))
+                           .pipe(reporter())
+                           .pipe(gulp.dest('src'));
