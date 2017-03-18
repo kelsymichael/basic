d51ba64f5012ebe00fc28a9d3f35f8e51ac284fa
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-03-18 11:47:39 -0600

    Add public folders

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+// express.static allows all routes to have
+// acces to what is in the /public folder
+app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 
 var server = app.listen(3000, function(){
