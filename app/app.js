3ed94e04900700389c7ed105f5b4dea7526bfe9a
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-03-18 11:22:48 -0600

    Add basic routing

--- app/app.js
+++ app/app.js
@@ -2,7 +2,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('<h1> testing express</h1>');
+  res.send('<h1>HOME</h1>');
+});
+
+app.get('/writings', function(req, res){
+  res.send('<h1>WRITINGS</h1>');
+});
+
+app.get('/doodles', function(req, res){
+  res.send('<h1>DOODLES</h1>');
+});
+
+app.get('/about', function(req, res){
+  res.send('<h1>ABOUT</h1>');
 });
 
 var server = app.listen(3000, function(){
