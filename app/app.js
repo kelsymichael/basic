36040fd75df044f8917b6c083ab9d4026658207c
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-03-20 08:17:18 -0600

    Attempt to fix stylus
    - Not working, compiles at runtime?
    - Probably remove and start simpler
    - Definitely not effective use of my time

--- app/app.js
+++ app/app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const reload  = require('reload');
 const app     = express();
+const stylus = require('stylus');
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'pug');
-app.set('views', 'app/views');
+app.set('views', __dirname + '/views');
 
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+}
+app.use(stylus.middleware({
+  src: __dirname + '/views',
+  // dest: __dirname + '/public',
+  debug: true,
+  force: true
+  // ,
+  // compile: function(str, path){
+  //   console.log('compiling');
+  //   return stylus(str)
+  //     .set('filename', path)
+  //     .set('warn', true)
+  //     .set('compress', true);
+  // }
+}))
 // express.static allows all routes to have
 // acces to what is in the /public folder
-app.use(express.static('app/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(require('./routes/index'));
 
 var server = app.listen(3000, function(){
